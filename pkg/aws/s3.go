562bb300308c09ac612355c246aca897bc27fd08
rafaapcode <rafaap2003@gmail.com>
2024-08-31 13:12:06 -0300

    feat: creating the UploadSingleFile to s3 funtion

--- pkg/aws/s3.go
+++ pkg/aws/s3.go
@@ -1,5 +1,62 @@
 package aws_s3
 
-func S3() {
+import (
+	"fmt"
+	"mime/multipart"
 
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3/s3manager"
+)
+
+func UploadSingleFile(bucketName, filepath string, file multipart.File) error {
+	sess, err := session.NewSession(&aws.Config{
+		Region: aws.String("us-east-1"),
+	})
+
+	if err != nil {
+		fmt.Println(err.Error())
+		return err
+	}
+
+	uploader := s3manager.NewUploader(sess)
+
+	_, err = uploader.Upload(&s3manager.UploadInput{
+		Bucket: aws.String(bucketName),
+		Key:    aws.String(filepath),
+		Body:   file,
+	})
+
+	if err != nil {
+		fmt.Println(err.Error())
+		return err
+	}
+
+	return nil
+}
+
+func UploadMultipleFiles(bucketName, filepath string, files []multipart.File) error {
+	sess, err := session.NewSession(&aws.Config{
+		Region: aws.String("us-east-1"),
+	})
+
+	if err != nil {
+		fmt.Println(err.Error())
+		return err
+	}
+
+	uploader := s3manager.NewUploader(sess)
+
+	_, err = uploader.Upload(&s3manager.UploadInput{
+		Bucket: aws.String(bucketName),
+		Key:    aws.String(filepath),
+		Body:   files[0],
+	})
+
+	if err != nil {
+		fmt.Println(err.Error())
+		return err
+	}
+
+	return nil
 }
